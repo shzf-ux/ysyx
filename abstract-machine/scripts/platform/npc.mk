17496ca5276475c5a235369300f7d83db54bc2a0
tracer-ysyx <tracer@ysyx.org>
2025-04-15 19:09:36 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:36 up 11:04,  1 user,  load average: 1.20, 1.21, 1.00

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,10 +12,11 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NPCFLAGS  += -b -d $(IMAGE).bin
+NPCFLAGS  +=-b -d $(NEMU_LIB_NAME)
 NEMU_LIB_NAME := /home/zzy/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so
 #$(abspath$(wildcard $(NEMU_HOME)/build/riscv32-nemu-interpreter-so))
-NPCFLAGS  +=$(NEMU_LIB_NAME)
+#-b无需参数，直接传递文件即可
+
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
