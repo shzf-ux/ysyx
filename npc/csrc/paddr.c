d244195b64bb81b99dd14b258da0e4db0db708ed
tracer-ysyx <tracer@ysyx.org>
2025-04-21 14:39:42 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:39:42 up  1:49,  1 user,  load average: 0.66, 0.29, 0.14

--- npc/csrc/paddr.c
+++ npc/csrc/paddr.c
@@ -38,7 +38,9 @@ extern "C" uint32_t pmem_readv(int raddr)
     
     uint32_t ret = pmem_read(raddr, 4);
     //printf("ret:%08x\n", ret);
+    #ifdef CONFIG_MTRACE
     display_memory_read(raddr, ret);
+    #endif
     return ret;
 }
 void init_mem()
@@ -48,16 +50,49 @@ void init_mem()
 extern "C" void pmem_write( int waddr,int wdata,uint8_t wmask)
 {
  
-
-    // mask 00001111
+//0001  0011  1111
+//0010  1100
+//0100         
+//1000
     // 把32为的data按照wmask写入addr里面
-    display_memory_write( waddr, wdata);
-    uint8_t *p = guest_to_host(waddr); // 获取8字节的指针
+#ifdef CONFIG_MTRACE
+    display_memory_write(waddr, wdata);
+#endif
+
+uint8_t *p = guest_to_host(waddr); // 获取8字节的指针
+
+if (wmask == 0x01 || wmask == 0x02 || wmask == 0x04 || wmask == 0x08)
+{
+
     for (int i = 0; i < 4; i++)
     {
-        if ((wmask >> i) & 1)//是1就写入
-            p[i] = (wdata >> (i * 8)) & 0xFF;//111111111//保留相关位
+        if ((wmask >> i) & 1)    // 是1就写入
+            p[i] = wdata & 0xFF; // 111111111//保留相关位
     }
+}
+else if (wmask == 0x03 || wmask == 0x0c)
+{
+
+    if (wmask == 0x03)
+    {
+        p[0] = wdata; // 111111111//保留相关位
+        p[1] = wdata >> 8;
+    }
+    else if (wmask == 0x03)
+    {
+        p[2] = wdata >> 16; // 111111111//保留相关位
+        p[3] = wdata >> 24;
+    }
+}
+else if (wmask == 0xf)
+{
+
+    // 是1就写入
+    p[0] = wdata >> 0;  // 111111111//保留相关位
+    p[1] = wdata >> 8;  // 111111111//保留相关位
+    p[2] = wdata >> 16; // 111111111//保留相关位
+    p[3] = wdata >> 24; // 111111111//保留相关位
+}
 }
 void display_memory_read(uint32_t addr, uint32_t data)
 {
