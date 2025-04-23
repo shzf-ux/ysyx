38e700a21990f5ceab03110a60794f8f0edc704e
tracer-ysyx <tracer@ysyx.org>
2025-04-23 15:43:09 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:09 up 15:10,  1 user,  load average: 1.13, 0.89, 0.92

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,14 +29,42 @@ int atoi(const char* nptr) {
   return x;
 }
 
+Area heapp = {
+    // 这里可以根据实际情况修改堆区起始地址
+    .start = (void *)0x10000000,
+    // 这里可以根据实际情况修改堆区结束地址
+    .end = (void *)0x20000000
+  };
+
+static void *addr = NULL;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if (addr == NULL)
+  {
+    addr = heapp.start;
+  }
+
+  // 检查是否有足够的空间分配
+  if ((char *)addr + size > (char *)heapp.end)
+  {
+    // 没有足够的空间，返回 NULL
+    return NULL;
+  }
+
+  void *allocated = addr;
+  // 更新 addr 到下一个可用位置
+  addr = (char *)addr + size;
+
+  return allocated;
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  //panic("Not implemented");
+  panic("Not implemented");
 #endif
-  return NULL;
+
+
+
+
 }
 
 void free(void *ptr) {
