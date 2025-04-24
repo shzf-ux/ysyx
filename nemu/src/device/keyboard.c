1a20eceea67b795b9bb7cde873e12182ee1d3f0e
tracer-ysyx <tracer@ysyx.org>
2025-04-24 14:51:18 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:18 up 20:04,  1 user,  load average: 2.35, 2.27, 2.00

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -68,7 +68,7 @@ static uint32_t key_dequeue() {
 }
 
 void send_key(uint8_t scancode, bool is_keydown) {
-  printf("scancode:%d\n", scancode);
+ 
   if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != NEMU_KEY_NONE)
   {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
@@ -91,6 +91,10 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
+  if(i8042_data_port_base[0]!=0)
+  {
+    printf("k:%d", i8042_data_port_base[0]);
+  }
 }
 
 void init_i8042() {
