50a980e33f887181390c990cc9e47e8c8111b6de
tracer-ysyx <tracer@ysyx.org>
2025-04-24 14:20:50 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:49 up 19:34,  1 user,  load average: 2.20, 1.61, 1.28

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -52,6 +52,7 @@ void device_update() {
         break;
 #ifdef CONFIG_HAS_KEYBOARD
       // If a key was pressed
+        printf("hello world\n");
       case SDL_KEYDOWN:
       case SDL_KEYUP: {
         uint8_t k = event.key.keysym.scancode;
