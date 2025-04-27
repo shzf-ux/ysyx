cce3b56070b99f31b887d3a5618370c14e09d1f7
tracer-ysyx <tracer@ysyx.org>
2025-04-27 15:32:23 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:23 up  7:52,  1 user,  load average: 0.41, 0.42, 0.43

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -99,7 +99,6 @@ void init_vga() {
 #endif
 
   vmem = new_space(screen_size());
- 
   add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
