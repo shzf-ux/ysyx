7e4cfc5623a7c8e9292606df89f4a9bd4793b72a
tracer-ysyx <tracer@ysyx.org>
2025-04-27 15:24:54 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:54 up  7:44,  1 user,  load average: 0.88, 0.64, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,7 +22,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 //AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像. 图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色. 若sync为true, 则马上将帧缓冲中的内容同步到屏幕上.
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
+  uint32_t *pixels = ctl->pixels;//获取像素
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;//读宽度
  
+      fb[screen_w ] = pixels[w * y + x];
+  
+  if (ctl->sync)
+  {
+    outl(SYNC_ADDR, 1);//
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
