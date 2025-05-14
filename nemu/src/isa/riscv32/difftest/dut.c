f200a3b5113c52f85fcbfb3fe390d25525681c37
tracer-ysyx <tracer@ysyx.org>
2025-05-14 14:20:08 +0800

    > compile NEMU 赵智阳 Linux yang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:08 up 1 day, 12:19,  1 user,  load average: 0.65, 0.66, 0.44

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,6 +18,11 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  char *regs[] = {
+      "x0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+      "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+      "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+      "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
   bool sign = true;
   int i = 0;
@@ -25,8 +30,8 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   {
     if (cpu.gpr[i] != ref_r->gpr[i])
     {
-      printf("wrong dut:%08x\n", cpu.gpr[i]);
-      printf("right ref:%08x\n", ref_r->gpr[i]);
+      printf("wrong dut %s:%08x\n", regs[i] ,cpu.gpr[i]);
+      printf("right ref %s:%08x\n", regs[i] ,ref_r->gpr[i]);
       sign = false;
       break;
     }
