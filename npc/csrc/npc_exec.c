03fb544c81d05081db4f1c574bc7c2735bd5a9e2
zzy <1148043178@qq.com>
2025-08-21 17:29:00 +0800

    1

--- npc/csrc/npc_exec.c
+++ npc/csrc/npc_exec.c
@@ -80,9 +80,9 @@ void npc_exec(uint64_t n)
             #endif
         }
         //soc_top->eval();
-       // vcd->dump(sim_time);
-        if(sim_time>1000000){
-    // return;
+    vcd->dump(sim_time);
+        if(sim_time>5000000){
+     return;
         }
         sim_time++;
    }
